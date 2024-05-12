6f0221ff364064564fc8b64ac363b420cf662aec
Matheus Borges <matob@live.com>
2024-05-12 01:49:29 -0300

    refactor: adjust workflows envs

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -44,8 +44,8 @@ resource "aws_db_instance" "default" {
   publicly_accessible = false
   multi_az            = false
   db_name             = var.db_name
-  username            = var.db_user
-  password            = var.db_password
+  username            = var.DB_USERNAME
+  password            = var.DB_PASSWORD
 
   db_subnet_group_name   = aws_db_subnet_group.default.name
   vpc_security_group_ids = [aws_security_group.rds.id]
